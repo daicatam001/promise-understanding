a90496f5e5e3cae884f5b6c474702d6f5ef30970
Tam Pham <tampt95@gmail.com>
2021-08-02 23:42:53 +0700

    update my promise

--- index.js
+++ index.js
@@ -1,36 +1,145 @@
-class MyPromise{
-    
-    _callBackList=[]
+class MyPromise {
+  _resolutionQueue = [];
+  _rejectionQueue = [];
+  _value;
+  _state = 'pending';
+  _rejectionReason;
 
-    constructor(executor){
-        executor(this.resolve.bind(this))    
+  constructor(executor) {
+    try {
+      executor(this._resolve.bind(this), this._reject.bind(this));
+    } catch (e) {
+      this._reject(e);
     }
+  }
 
-    resolve(value){
-       while(this._callBackList.length > 0){
-           const resolution = this._callBackList.shift()
-           const returnValue = resolution.handler(value)
-
-           if(returnValue && returnValue instanceof MyPromise){
-               returnValue.then(v=>{
-                   resolution.promise.resolve(v)
-               })
-           }else{
-            resolution.promise.resolve(value)
-           }
-       }
+  _runResolutionHandlers() {
+    while (this._resolutionQueue.length > 0) {
+      const resolution = this._resolutionQueue.shift();
+      try {
+        const returnValue = resolution.handler(this._value);
+        if (returnValue && returnValue instanceof MyPromise) {
+          returnValue
+            .then((v) => {
+              resolution.promise._resolve(v);
+            })
+            .catch((e) => {
+              resolution.promise._reject(e);
+            });
+        } else {
+          resolution.promise._resolve(returnValue);
+        }
+      } catch (e) {
+        resolution.promise._reject(e);
+      }
     }
+  }
 
-    then(callback){
-        const newPromise = new MyPromise(function(){})
-
-        this._callBackList.push({
-            handler:callback,
-            promise:newPromise
-        })
-        return newPromise;
+  _runRejectionHandlers() {
+    while (this._rejectionQueue.length > 0) {
+      const resolution = this._rejectionQueue.shift();
+      try {
+        const returnValue = resolution.handler(this._rejectionReason);
+        if (returnValue && returnValue instanceof MyPromise) {
+          returnValue
+            .then((v) => {
+              resolution.promise._resolve(v);
+            })
+            .catch((e) => {
+              resolution.promise._reject(e);
+            });
+        } else {
+          resolution.promise._resolve(returnValue);
+        }
+      } catch (e) {
+        resolution.promise._reject(e);
+      }
     }
+  }
 
+  _resolve(value) {
+    if (this._state === 'pending') {
+      this._value = value;
+      this._state = 'resolved';
+      this._runResolutionHandlers();
+    }
+  }
+
+  _reject(reason) {
+    if (this._state === 'pending') {
+      this._rejectionReason = reason;
+      this._state = 'rejected';
+      this._runRejectionHandlers();
+      while (this._resolutionQueue.length > 0) {
+        const resolution = this._resolutionQueue.shift();
+        resolution.promise._reject(this._rejectionReason);
+      }
+    }
+  }
+
+  then(callback, rejectionHandler) {
+    const newPromise = new MyPromise(function () {});
+
+    this._resolutionQueue.push({
+      handler: callback,
+      promise: newPromise,
+    });
+    if (typeof rejectionHandler === 'function') {
+      this._rejectionQueue.push({
+        handler: rejectionHandler,
+        promise: newPromise,
+      });
+    }
+    if (this._state === 'resolved') {
+      this._runResolutionHandlers();
+    }
+    if (this._state === 'rejected') {
+      newPromise._reject(this._rejectionReason);
+    }
+    return newPromise;
+  }
+  catch(callback) {
+    const newPromise = new MyPromise(function () {});
+
+    this._rejectionQueue.push({
+      handler: callback,
+      promise: newPromise,
+    });
+    if (this._state === 'rejected') {
+      this._runRejectionHandlers();
+    }
+    return newPromise;
+  }
 }
 
+// const bar = () => console.log('bar');
+// const boo = () => console.log('boo');
+
+// const foo = () => {
+//   console.log('foo');
+//   setTimeout(bar, 0);
+
+//   new MyPromise((resolve) => {
+//     resolve('far');
+//   }).then((res) => console.log(res));
+
+//   boo();
+// };
+
+// const foo1 = () => {
+//   console.log('foo1');
+//   setTimeout(bar, 0);
+
+//   new Promise((resolve) => {
+//     resolve('far');
+//   }).then((res) => console.log(res));
+
+//   boo();
+// };
+
+// foo();
+
+// console.log('---------------------');
+
+// foo1();
 module.exports = MyPromise;
--- spec/index.spec.js
+++ spec/index.spec.js
@@ -1,75 +1,275 @@
-const MyPromise = require('../index')
+const MyPromise = require('../index');
 
-describe('test MyPromise',function(){
-    it('executor was called after initialization', function(){
-        let string;
-        new MyPromise(()=>{
-            string = 'foo'
-        })
+describe('test MyPromise', function () {
+  it('executor was called after initialization', function () {
+    let string;
+    new MyPromise(() => {
+      string = 'foo';
+    });
 
-        expect(string).toBe('foo')
-    })
-    it('resolution handler í called when promise is resolve',function(){
-       const testString ='foo';
-       const promise = new MyPromise((resolve)=>{
-           setTimeout(()=>{
-                resolve(testString)
-           })
-       }) 
-       promise.then((string)=>{
-           expect(string).toBe(testString)
-       })
-    })
+    expect(string).toBe('foo');
+  });
+  it('resolution handler í called when promise is resolve', function () {
+    const testString = 'foo';
+    const promise = new MyPromise((resolve) => {
+      setTimeout(() => {
+        resolve(testString);
+      });
+    });
+    promise.then((string) => {
+      expect(string).toBe(testString);
+    });
+  });
 
-    it('promise supports many resolutions',function(){
-        const testString ='foo';
-        const promise = new MyPromise((resolve)=>{
-            setTimeout(()=>{
-                 resolve(testString)
-            },100)
-        }) 
-        promise.then((string)=>{
-            expect(string).toBe(testString)
-        })
-        promise.then((string)=>{
-            expect(string).toBe(testString)
-        })
-    })
+  it('promise supports many resolutions', function () {
+    const testString = 'foo';
+    const promise = new MyPromise((resolve) => {
+      setTimeout(() => {
+        resolve(testString);
+      }, 100);
+    });
+    promise.then((string) => {
+      expect(string).toBe(testString);
+    });
+    promise.then((string) => {
+      expect(string).toBe(testString);
+    });
+  });
 
-    // hàm then luôn trả về 1 promise mới, promise sẽ resolve giá trị
-    // mà promise ban đầu đã resolve hoặc 1 giá trị đươc resolve từ 
-    // 1 promise trả về trong hàm then trước đó
-    it('resolution handlers can be chained',function(){
-        const testString ='foo';
-        const promise = new MyPromise((resolve)=>{
-            setTimeout(()=>{
-                 resolve()
-            },100)
-        })
+  // hàm then luôn trả về 1 promise mới, promise sẽ resolve giá trị
+  // return hàm them trước đó hoặc 1 giá trị đươc resolve từ
+  // 1 promise trả về trong hàm then trước đó
+  it('resolution handlers can be chained', function () {
+    const testString = 'foo';
+    const promise = new MyPromise((resolve) => {
+      setTimeout(() => {
+        resolve();
+      }, 100);
+    });
 
-        promise.then(()=>{
-            return new MyPromise((resolve)=>{
-                setTimeout(()=>{
-                    resolve(testString)
-                }) 
-            })
-        }).then((string)=>{
-            expect(string).toBe(testString)
-        })
-    })
-    it('chaining works with non-promise return values', function(){
-        const  testString ='foo'
-        
-        const promise = new MyPromise((resolve)=>{
-            setTimeout(()=>{
-                 resolve()
-            },100)
-        })
+    promise
+      .then(() => {
+        return new MyPromise((resolve) => {
+          setTimeout(() => {
+            resolve(testString);
+          });
+        });
+      })
+      .then((string) => {
+        expect(string).toBe(testString);
+      });
+  });
+  it('chaining works with non-promise return values', function (done) {
+    const testString = 'foo';
 
-        promise.then(()=>{
-           return testString
-        }).then((string)=>{
-            expect(string).toBe(testString)
-        })
-    })
-})
+    const promise = new MyPromise((resolve) => {
+      setTimeout(() => {
+        resolve();
+      }, 100);
+    });
+
+    promise
+      .then(() => {
+        return testString;
+      })
+      .then((string) => {
+        expect(string).toBe(testString);
+        done();
+      });
+  });
+  it('resolution handlers can be attached when promise is resolved', function (done) {
+    const testString = 'foo';
+    const promise = new MyPromise((resolver) => {
+      setTimeout(() => {
+        resolver(testString);
+      }, 100);
+    });
+    promise.then(function () {
+      setTimeout(function () {
+        promise.then(function (string) {
+          expect(string).toBe(testString);
+          done();
+        });
+      }, 100);
+    });
+  });
+  it('calling resolve second time has no effect', function (done) {
+    const testString = 'foo';
+    const testString2 = 'bar';
+    const promise = new MyPromise((resolve) => {
+      setTimeout(function () {
+        resolve(testString);
+        resolve(testString2);
+      }, 100);
+    });
+    promise.then(function (value) {
+      expect(value).toBe(testString);
+      setTimeout(function () {
+        promise.then(function (value) {
+          expect(value).toBe(testString);
+          done();
+        });
+      }, 100);
+    });
+  });
+
+  it('rejection handler is called when promise is rejected', function (done) {
+    const testError = new Error('Something went wrong');
+
+    const promise = new MyPromise(function (resolve, reject) {
+      setTimeout(function () {
+        reject(testError);
+      }, 100);
+    });
+
+    promise.catch(function (value) {
+      expect(value).toEqual(testError);
+      done();
+    });
+  });
+  it('rejections are passed downstream', function (done) {
+    const testError = new Error('Something went wrong');
+
+    const promise = new MyPromise(function (resolve, reject) {
+      setTimeout(function () {
+        reject(testError);
+      }, 100);
+    });
+
+    promise
+      .then(function (value) {
+        return new MyPromise(function (resolve, reject) {
+          setTimeout(function () {
+            resolve('error');
+          });
+        });
+      })
+      .catch(function (value) {
+        expect(value).toEqual(testError);
+        done();
+      });
+  });
+  it('rejecting promises returned from resolution handlers are caught properly', function (done) {
+    const testError = new Error('Something went wrong');
+
+    const promise = new MyPromise(function (resolve, reject) {
+      setTimeout(function () {
+        resolve();
+      }, 100);
+    });
+
+    promise
+      .then(function (value) {
+        return new MyPromise(function (resolve, reject) {
+          setTimeout(function () {
+            reject(testError);
+          }, 100);
+        });
+      })
+      .catch(function (value) {
+        expect(value).toEqual(testError);
+        done();
+      });
+  });
+
+  it('rejection handlers catch synchronous errors in resolution handlers', function (done) {
+    const testError = new Error('Something went wrong');
+
+    const promise = new MyPromise(function (resolve, reject) {
+      setTimeout(function () {
+        resolve();
+      }, 100);
+    });
+
+    promise
+      .then(function (value) {
+        throw testError;
+      })
+      .catch(function (value) {
+        expect(value).toEqual(testError);
+        done();
+      });
+  });
+  it('rejection handlers catch synchronous errors in the executor function', function (done) {
+    const testError = new Error('Something went wrong');
+
+    const promise = new MyPromise(function (resolve, reject) {
+      throw testError;
+    });
+
+    promise
+      .then(function (value) {
+        return new MyPromise(function (resolve, reject) {
+          setTimeout(function () {
+            resolve();
+          }, 100);
+        });
+      })
+      .catch(function (value) {
+        expect(value).toEqual(testError);
+        done();
+      });
+  });
+
+  it('rejection handler catch synchronous error', function (done) {
+    const testError = new Error('Something went wrong');
+    const promise = new MyPromise(function (resolve, reject) {
+      setTimeout(function () {
+        resolve();
+      }, 100);
+    });
+
+    promise
+      .then(function (value) {
+        throw new Error('some error');
+      })
+      .catch(function () {
+        throw testError;
+      })
+      .catch(function (value) {
+        expect(value).toEqual(testError);
+        done();
+      });
+  });
+  it('rejection promises returned from rejection handlers are caught', function (done) {
+    const testError = new Error('Something went wrong');
+    const promise = new MyPromise(function (resolve, reject) {
+      setTimeout(function () {
+        resolve();
+      }, 100);
+    });
+
+    promise
+      .then(function (value) {
+        throw new Error('some error');
+      })
+      .catch(function () {
+        return new MyPromise(function (resolve, reject) {
+          setTimeout(function () {
+            reject(testError);
+          }, 100);
+        });
+      })
+      .catch(function (value) {
+        expect(value).toEqual(testError);
+        done();
+      });
+  });
+
+  it('second argument in then is treated as a rejection handler', function (done) {
+    const testError = new Error('Something went wrong');
+    const promise = new MyPromise(function (resolve, reject) {
+      setTimeout(function () {
+        reject(testError);
+      }, 100);
+    });
+
+    promise.then(
+      function (value) {},
+      function (e) {
+        expect(e).toEqual(testError);
+        done();
+      }
+    );
+  });
+});
